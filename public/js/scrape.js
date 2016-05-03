d1d96958320b2f641dc6dc353d08341c28951a28
takefumi mitsui <mitake.ck@gmail.com>
2016-05-03 21:19:33 +0900

    同列 dom に対して hover 時にカラーハイライト

--- public/js/scrape.js
+++ public/js/scrape.js
@@ -68,13 +68,64 @@ $(function() {
 
     var $column = $("#column");
     var $selecter = $("#selecter");
+
+    var getCSSSelecter = function(tagName, className){
+      if(className === ""){
+        return tagName;
+      }else{
+        return tagName + "." + className;
+      }
+    };
+
     $("*", $content).click(function(event) {
       console.log(event);
       console.log($(this).text());
+      console.log($(this).selector);
       $column.val($(this).text());
-      $selecter.val($(this).prop("tagName") + "." + $(this).prop("class").split(" ").join("."));
+      $selecter.val(getCSSSelecter($(this).prop("tagName"), $(this).prop("class").split(" ").join(".")));
       return false;
     });
+
+    var cache;
+    var tagName;
+    var className;
+    var $paerElement;
+    var isSearch = false;
+
+    $("p, span, a, li, h1, h2, h3, h4, h5, h6, h7, cite", $content).hover(
+      function(event){
+        if(isSearch) return;
+        console.log($(this).length);
+        var depthLimit = 10;
+        var currentDepth = 0;
+        var minPearTh = 3;
+        CssCache = $(this).css("background");
+        tagName = $(this)[0].tagName;
+        className = ($(this)[0].className).split(" ").join(".");
+        // console.log("tagName : " + tagName);
+        // console.log("className : " + className);
+        var $parentElement;
+        do{
+          $parentElement = $parentElement === undefined ? $(this)[0].parentElement : $parentElement.parentElement;
+          console.log("-------------");
+          console.log("search term : " + currentDepth);
+          console.log(getCSSSelecter(tagName, className));
+          console.log($parentElement);
+          $paerElement = $(getCSSSelecter(tagName, className), $parentElement);
+          console.log("pear count : " + $paerElement.length);
+          console.log("-------------");
+          currentDepth ++;
+        }while(currentDepth <= depthLimit && ($paerElement && $paerElement.length <= minPearTh));
+        $paerElement.css("background", "#6F00FF");
+        isSearch = true;
+        // console.log($paerElement);
+      },
+      function(event){
+        $paerElement.css("background", CssCache);
+        $paerElement = undefined;
+        isSearch = false;
+      }
+    );
 
     setTimeout(function() {
       $("#wrapper").toggleClass("toggled");
